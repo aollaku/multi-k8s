a8b8226ee54432d314113156c2d6a9d96233d7b0
Andi Collaku <aollaku@gmail.com>
2025-06-12 04:05:35 +0100

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            This is React Multi Container Kubernetes.
+            This application is uploaded to Google Cloud Kubernetes.
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
